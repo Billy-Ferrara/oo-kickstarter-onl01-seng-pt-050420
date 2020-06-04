0179510961defef4867ce3e8e664eed7b8294337
Billy Ferrara <willfira717@gmail.com>
2020-06-04 19:33:34 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,11 +1,15 @@
 class Project
 
   attr_reader :title
-  attr_accessor :backers 
+  attr_accessor :backers
 
   def initialize(title)
     @title = title
     @backers = []
+  end
+
+  def backed_projects(project)
+    @backers << projects
   end
 
 
